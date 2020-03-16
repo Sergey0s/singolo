c8f9181b7badc1a796ebc65299092465f75407a0
Sergey0s <mediaworld@mail.ru>
2020-03-16 13:14:17 +0300

    feat: add slider

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-const MENU = document.getElementById('header_menu');
-
-
-MENU.addEventListener('click', (event) => {
-    console.log(MENU.querySelectorAll('.header__link'));
-    MENU.querySelectorAll('.header__link').forEach(el => el.classList.remove('header__active'));
+const menu = document.getElementById('header_menu');
+menu.addEventListener('click', (event) => {
+    menu.querySelectorAll('.header__link').forEach(el => el.classList.remove('header__active'));
     event.target.classList.add('header__active')
-})
+});
+
+
+const slider = document.getElementById('slider');
+slider.querySelectorAll('.slider__arrow').forEach(arrow => {
+    arrow.addEventListener('click', function () {
+        let slider2 = document.querySelector('.slider2__content');
+        let slider1 = document.querySelector('.slider__content');
+        slider.querySelector('.slider__wrapper').classList.toggle('slider2_bg');
+        if (slider2.style.display === "none") {
+            slider1.style.display = "none";
+            slider2.style.display = "flex";
+        } else {
+            slider1.style.display = "flex";
+            slider2.style.display = "none";
+        }
+    });
+});
+
+
+
