3cc9f2c949f28dcf404e2ef40cf59f976bf48028
Sergey0s <mediaworld@mail.ru>
2020-03-16 16:20:14 +0300

    feat: complete singoloDOM task

--- script.js
+++ script.js
@@ -51,10 +51,57 @@ function reorderPictures() {
 }
 
 portfolio.addEventListener('click', (event => {
-    console.log(event.target)
     if (event.target.className !== 'portfolio__works') {
         portfolio.querySelectorAll('.portfolio__image').forEach(el => el.classList.remove('portfolio__image-active'));
         event.target.classList.add('portfolio__image-active')
     }
-
 }));
+
+
+const form = document.querySelector("form");
+const submitFormButton = document.querySelector(".form__button");
+const modalBackdrop = document.querySelector(".modal-backdrop");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const subjectInput = document.querySelector("#subject");
+const textareaInput = document.querySelector("#description");
+
+const modalSubjectElement = document.querySelector("#modal-subject");
+const modalDescriptionElement = document.querySelector("#modal-description");
+const modalCloseButton = document.querySelector("#modal-close-button");
+
+
+submitFormButton.addEventListener("click", event => {
+    event.preventDefault();
+
+    if (!nameInput.value) {
+        nameInput.classList.add("invalid");
+    }
+
+    if (!emailInput.value) {
+        emailInput.classList.add("invalid");
+    }
+
+    if (nameInput.value && emailInput.value) {
+        modalBackdrop.classList.remove("visually-hidden");
+        modalSubjectElement.innerText = subjectInput.value
+            ? `Subject: ${subjectInput.value}`
+            : "Without subject";
+        modalDescriptionElement.innerText = textareaInput.value
+            ? `Description: ${textareaInput.value}`
+            : "Without description";
+    }
+});
+
+modalCloseButton.addEventListener("click", () => {
+    modalBackdrop.classList.add("visually-hidden");
+    form.reset();
+});
+
+nameInput.addEventListener("focus", function() {
+    this.classList.remove("invalid");
+});
+
+emailInput.addEventListener("focus", function() {
+    this.classList.remove("invalid");
+});
